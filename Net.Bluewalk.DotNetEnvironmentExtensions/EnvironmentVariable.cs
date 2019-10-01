b7352a490ccc9e891107dd070c0aeff9cf70bafa
Délano <delano.reijnierse@zrefinery.nl>
2019-10-01 11:37:02 +0200

    Initial commit

---
+++ Net.Bluewalk.DotNetEnvironmentExtensions/EnvironmentVariable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Net.Bluewalk.DotNetEnvironmentExtensions
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EnvironmentVariable : Attribute
+    {
+        public string Name { get; set; }
+        public object Default { get; set; }
+    }
+}
